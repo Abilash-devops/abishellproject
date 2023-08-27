7fcdd3bbdfdcd059278f14effd285ab67b8c73c2
Abilash-devops <abilashdevopsaws@gmail.com>
2023-08-27 19:36:51 +0530

    awsscript

--- script_ec2.sh
+++ script_ec2.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 NAMES=("dev" "QA" "preprod" "prod")
+INSTANCE_TYPE=""
+IMAGE_ID=ami-03265a0778a880afb
+SECUIRTY_GROUP=sg-00e1606b168b9215a
 
 for i in "${NAMES[@]}"
 do
-    echo "NAME :$i"
+    if [[ $i == preprod || $i == prod ]]
+    then
+    INSTANCE_TYPE="t3.medium"
+    else
+    INSTACE_TYPE="t2.micro"
+    echo "Creating $i instance"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress') 
+    echo "created $i instance: $IP_ADDRESS"
 done
